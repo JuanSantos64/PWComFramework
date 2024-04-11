513f71babfe878b391e1bf37fb409d36080089f4
Juan Santos <107580945+JuanSantos64@users.noreply.github.com>
2024-04-10 21:55:49 -0300

    Commit Inicial

---
+++ epictask/assets/js/carregar.js
@@ -0,0 +1,22 @@
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+console.log(tarefas)
+
+tarefas.forEach( (tarefa) => {
+    const card = document.createElement("div")
+    card.classList.add("nes-container", "with-title", "is-centered")
+    card.innerHTML = `
+    <p class="title">${tarefa.titulo}</p>
+    <p>${tarefa.descricao}</p>
+    <a href="#" class="nes-badge is-icon">
+        <span class="is-warning"><i class="nes-icon coin is-small"></i></span>
+        <span class="is-primary">${tarefa.pontos}</span>
+      </a>
+      <progress class="nes-progress is-success" value="70" max="100"></progress>
+      
+          <button type="button" class="nes-btn is-error">Apagar</button>
+          <button type="button" class="nes-btn is-primary">-</button>
+          <button type="button" class="nes-btn is-primary">+</button>
+    `
+    document.querySelector("#lista-de-tarefas").appendChild(card)
+})
